eee950a7b0a62a3153f0e1d1326f2a0912d87a0e
tan095297 <tan.yout001@gmail.com>
2022-01-25 17:43:59 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/shared/navigation/navigation.component';
+import { MainComponent } from './components/pages/main/main.component';
+import { ProductComponent } from './components/pages/product/product.component';
+import { NewProductComponent } from './components/pages/new-product/new-product.component';
+import { EditProductComponent } from './components/pages/edit-product/edit-product.component';
+import { ProductDetailComponent } from './components/pages/product-detail/product-detail.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    MainComponent,
+    ProductComponent,
+    NewProductComponent,
+    EditProductComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/pages/product-detail/product-detail.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit {
+  id:any;
+  currentProduct : any ;
+
+
+  constructor(private service: ProductService , private activatedRouter : ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.activatedRouter.params.subscribe(params=>{
+        this.id=params['id'];
+    });
+
+    this.service.getProductById(this.id).subscribe((res)=>{
+      this.currentProduct = res.data;
+    });
+  }
+
+}
